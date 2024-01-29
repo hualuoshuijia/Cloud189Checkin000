61bc24209100ddf051b739954557ae20a82e9f13
hualuoshuijia <49584289+hualuoshuijia@users.noreply.github.com>
2024-01-29 11:00:16 +0800

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,16 +1,25 @@
 module.exports = [{
   userName: process.env.TY_USER_NAME || 'userName',
   password: process.env.TY_PASSWORD || 'password',
-}, {
+}， {
   userName: process.env.TY_USER_NAME1,
   password: process.env.TY_PASSWORD1,
-}, {
+}， {
   userName: process.env.TY_USER_NAME2,
   password: process.env.TY_PASSWORD2,
-}, {
+}， {
   userName: process.env.TY_USER_NAME3,
   password: process.env.TY_PASSWORD3,
-}, {
-  userName: process.env.TY_USER_NAME4,
-  password: process.env.TY_PASSWORD4,
+}， {
+  userName: process.env.TY_USER_NAME4，
+  password: process.env.TY_PASSWORD4，
+}， {
+  userName: process.env.TY_USER_NAME5，
+  password: process.env.TY_PASSWORD5，
+}， {
+  userName: process.env.TY_USER_NAME6，
+  password: process.env.TY_PASSWORD6，
+}， {
+  userName: process.env.TY_USER_NAME7，
+  password: process.env.TY_PASSWORD7，
 }];
